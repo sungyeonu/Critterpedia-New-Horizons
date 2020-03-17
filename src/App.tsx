a2ea925b989a0574b1946e192435ccc62c95368b
Sungyeon <sungyeonu@gmail.com>
2020-03-16 23:36:44 -0400

    :rocket:

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import IconsContainer from './components/IconsContainer'
-import AppBar from './components/AppBar'
 import FishTable from './components/FishTable'
 import BugsTable from './components/BugsTable'
 
@@ -9,7 +8,7 @@ import BugsTable from './components/BugsTable'
 // }
 const [category, setCategory] = useState("fish");
 
-function App({setCategory}: any) {
+function App() {
   const [isToggled, setToggled] = useState(true);
   const toggleTrueFalse = () => setToggled(!isToggled);
 
@@ -19,13 +18,12 @@ function App({setCategory}: any) {
 
   return (
     <div className="App">
-      {/* <AppBar/> */}
       <button onClick={toggleTrueFalse}>
         <h3>Toggle me</h3>
       </button>
       <IconsContainer 
         // {...IconsContainerProps}
-        setCategory={setCategory}
+        // setCategory={setCategory}
       />
       {
         isToggled
