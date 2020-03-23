dae735478804aa107b8595aefbacc35c87c6a911
Sungyeon <sungyeonu@gmail.com>
2020-03-22 22:23:51 -0400

    Updates

---
+++ precache-manifest.ccd4f13595babe21264fc3978ec495f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32441f681443b4ea87388e588dad0fef",
+    "url": "/animal-crossing-db/index.html"
+  },
+  {
+    "revision": "c4c8ebf09e545d3a1fc5",
+    "url": "/animal-crossing-db/static/js/2.01ee4616.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/animal-crossing-db/static/js/2.01ee4616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a29483803122384073f",
+    "url": "/animal-crossing-db/static/js/main.22005fc7.chunk.js"
+  },
+  {
+    "revision": "edb8b0fad8ffbb208f77",
+    "url": "/animal-crossing-db/static/js/runtime-main.7a455bfe.js"
+  },
+  {
+    "revision": "806efd53b2a6587f5c80ed4bef55f18d",
+    "url": "/animal-crossing-db/static/media/bug.806efd53.png"
+  },
+  {
+    "revision": "89bb41e2457fd7f877edf3e3438d6447",
+    "url": "/animal-crossing-db/static/media/fish.89bb41e2.png"
+  }
+]);
